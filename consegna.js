9652df4e3645ad1eee013e099eb53cffecd2da94
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-10 23:24:58 +0200

    feat: homework

--- consegna.js
+++ consegna.js
@@ -4,3 +4,29 @@
 //     Estrarre la proprietà userId dalla ricetta
 //     Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
 //     Restituire la data di nascita dello chef
+
+
+async function getChefBirthday(id) {
+    try {
+      const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+
+      if (!recipeResponse.ok) {
+        throw new Error("Errore nel recupero della ricetta");
+      }
+
+      const recipe = await recipeResponse.json();
+      const userId = recipe.userId;
+
+      const userResponse = await fetch(`https://dummyjson.com/users/${userId}`);
+
+      const user = await userResponse.json();
+
+      return user.birthDate;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getChefBirthday(1)
+    .then((birthday) => console.log("Data di nascita dello chef:", birthday))
+    .catch((error) => console.error("Errore:", error.message));
